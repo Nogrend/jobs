5aa3a491c93e08311116ac4e379cc41bbdcd92f9
bram <nogrend@msn.com>
2024-07-13 19:05:52 +0200

    init

--- src/main/java/nogrend/jobs/helloWorldjob/HelloWorldJob.java
+++ src/main/java/nogrend/jobs/helloWorldjob/HelloWorldJob.java
@@ -2,7 +2,6 @@ package nogrend.jobs.helloWorldjob;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 public class HelloWorldJob implements Job {
 
