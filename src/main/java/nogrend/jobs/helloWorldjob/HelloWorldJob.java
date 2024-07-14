6c8c34109b4a7769ee82806684455292ddfaf5ad
bram <nogrend@msn.com>
2024-07-14 11:52:24 +0200

    add example logging levels

--- src/main/java/nogrend/jobs/helloWorldjob/HelloWorldJob.java
+++ src/main/java/nogrend/jobs/helloWorldjob/HelloWorldJob.java
@@ -2,15 +2,23 @@ package nogrend.jobs.helloWorldjob;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorldJob implements Job {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloWorldJob.class.getName());
 
     private static int counter = 0;
 
     @Override
     public void execute(JobExecutionContext context) {
         System.out.println(counter++ + " hello world");
-        System.out.println("Name of Job:     " + context.getJobDetail().getKey().getName());
-        System.out.println("Name of Trigger: " + context.getTrigger().getKey().getName());
+
+        var jobName = context.getJobDetail().getKey().getName();
+        logger.info("Name of Job: {}", jobName);
+        logger.debug("Name of Job: {}", jobName);
+        logger.warn("Name of Job: {}", jobName);
+        logger.error("Name of Job: {}", jobName);
     }
 }
